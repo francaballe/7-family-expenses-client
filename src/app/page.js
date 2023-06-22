76a3f32359ecbf3174c0af162dbdbbf08f8cd7af
Francisco Javier Caballe <francaballe@gmail.com>
2023-06-21 22:48:30 -0300

    making it a little bit responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import React from 'react';
 import Box from '@mui/material/Box';
 import Head from 'next/head';
 import CurrentMonthAndYear from './components/CurrentMonthAndYear';
@@ -6,34 +7,42 @@ import MyDataGrid from './components/MyDataGrid';
 import styles from './page.module.css';
 import theme from "./theme";
 import { ThemeProvider } from '@mui/material/styles';
-
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 export default function Home() {
-  
-  //Handlers
-  const handleSearch = (searchValue) => {
-    console.log('Search value:', searchValue);
-  };
-
 
   return (
     <ThemeProvider theme={theme}>
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div className={styles.container}>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <div className={styles.content}>
-          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: theme.palette.primary.main, my:1, py:1}}>
-            <CurrentMonthAndYear />            
-          </Box>
-          <MyDataGrid />
-        </div>
-      </main>
-    </div>
+        <main className={styles.main}>
+          <Container maxWidth="lg">
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: theme.palette.primary.main,
+                my: 1,
+                py: 1,
+              }}
+            >
+              <CurrentMonthAndYear />
+            </Box>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <MyDataGrid />
+              </Grid>
+            </Grid>
+          </Container>
+        </main>
+      </div>
     </ThemeProvider>
   );
 }
+
